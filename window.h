b8ad9dc268fae5b249605ed2486feb6494112b82
Carmel <carmelbarkaee032708@gmail.com>
2025-05-28 18:01:01 -0400

    decoupled cpu cycle and framerate

--- window.h
+++ window.h
@@ -6,14 +6,19 @@ constexpr int SCR_SCL = 35;
 constexpr int WIDTH = 64 * SCR_SCL;
 constexpr int HEIGHT = 32 * SCR_SCL;
 
+constexpr int CPU_HZ = 500;
+constexpr int MS_PER_CPU_CYCLE = 1000/CPU_HZ;
+
 constexpr int SCREEN_FPS = 60;
-constexpr int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
-static uint64_t next_time;
+constexpr int MS_PER_TIMER_CYLE = 1000 / SCREEN_FPS;
+
 struct Window {
     SDL_Window *win = NULL;
 	SDL_Renderer *renderer = NULL;
     
     Memory mem{};
+
+    
 
 
     void start() {
@@ -28,7 +33,7 @@ struct Window {
         mem.program_counter += 2;
         // Decode
         interpret_instruction(instruction, & mem);
-        tick(&mem);
+        
     }
 
     void render_square(SDL_Renderer* renderer, int x, int y) {
@@ -50,11 +55,6 @@ struct Window {
         }
     }
 
-    uint64_t time_left() {
-        uint64_t now = SDL_GetTicks();
-        if(next_time <= now) {return 0;}
-        return next_time - now;
-    }
     void init() {
         if (!SDL_Init(SDL_INIT_VIDEO)){
             std::cout << "BRUH" << SDL_GetError();
@@ -64,16 +64,40 @@ struct Window {
 	
         win = SDL_CreateWindow("Chip 8 Emulator", WIDTH, HEIGHT, 0);
         renderer = SDL_CreateRenderer(win, NULL);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        //SDL_SetRenderVSync(renderer, 1);
     }
 
     void run() {
         bool quit = false;
-        next_time = SDL_GetTicks() + SCREEN_TICKS_PER_FRAME;
-
+        double cpu_acc{}, timer_acc{};
+        uint64_t last_time = SDL_GetPerformanceCounter();
+        //next_time = SDL_GetTicks() + SCREEN_TICKS_PER_FRAME;
         while (!quit) {
+            Uint64 start = SDL_GetPerformanceCounter();
+            double change_in_time = (start - last_time) * 1000.0 / SDL_GetPerformanceFrequency();
+            last_time = start;
 
-            uint64_t now = SDL_GetTicks();
+            cpu_acc += change_in_time;
+            timer_acc += change_in_time;
+
+            while (cpu_acc >= MS_PER_CPU_CYCLE) {
+                frame();
+                cpu_acc -= MS_PER_CPU_CYCLE;
+            }
+
+            if (timer_acc >= MS_PER_TIMER_CYLE) {
+                tick(&mem);
+
+                if (!SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255)) {
+                std::cout << SDL_GetError() << "\n lmfao";
+                std::cout << "line";
+                }
+                SDL_RenderClear(renderer);
+                render_screen();    
+                SDL_RenderPresent(renderer);
+                
+                timer_acc -= MS_PER_TIMER_CYLE;
+            }
 
             SDL_Event e;
             if ( SDL_PollEvent(&e) ) {
@@ -85,21 +109,20 @@ struct Window {
                 }
             }
 
-            frame();
-            if (!SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255)) {
-                std::cout << SDL_GetError() << "\n lmfao";
-                std::cout << "line";
-            }
-            SDL_RenderClear(renderer);
+            /*frame();
+            tick(&mem);
 
-            render_screen();
 
-            SDL_RenderPresent(renderer);
+            
+            
 
             
 
-            //SDL_Delay(time_left()/  100);
-            next_time += SCREEN_TICKS_PER_FRAME;
+
+            //Uint64 end = SDL_GetPerformanceCounter();
+            //double elapsedMS = (end - start) * 1000.0 / SDL_GetPerformanceFrequency();
+            double time_to_wait = SCREEN_TICKS_PER_FRAME - elapsedMS;
+            if (time_to_wait > 0) SDL_Delay(time_to_wait);*/
         }
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(win);
